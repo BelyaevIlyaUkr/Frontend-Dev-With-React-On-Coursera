294002227a1197af5b8c146d91e168c56317832e
Ilya Belyaev <60135683+BelyaevIlyaUkr@users.noreply.github.com>
2020-04-09 14:05:51 +0300

    Functional Components

--- Second Week and Second Assignment/React/confusion/src/components/DishDetailedComponent.js
+++ Second Week and Second Assignment/React/confusion/src/components/DishDetailedComponent.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card,CardImg,CardBody,CardText,CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
 
-
-    renderDish(dish){
+    function RenderDish({dish}){
         if (dish != null){
             return(
                 <div className = "container">
@@ -27,7 +25,7 @@ class DishDetail extends Component {
         }
     }
 
-    renderComments(comments){
+    function RenderComments({comments}){
         const readyComments = comments.map((commment)=> {
             return(
                 <li key = {commment.id}>
@@ -58,16 +56,16 @@ class DishDetail extends Component {
         }
     } 
     
-    render() {
-        if (this.props.dish != null){
+    const DishDetail = (props) => {
+        if (props.dish != null){
             return(
                 <div className = "container">
                     <div className = "row">
                         <div className = "col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
+                            <RenderDish dish={props.dish} />
                         </div>
                         <div className = "col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
+                            <RenderComments comments={props.dish.comments} />
                         </div>
                     </div>
                 </div>
@@ -79,7 +77,5 @@ class DishDetail extends Component {
             )
         }
     }
-
-}
 
 export default DishDetail;
