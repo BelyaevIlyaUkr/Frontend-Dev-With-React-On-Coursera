4a31febc1ff0bcd035daf0a80f9b8e88fd037e8e
Ilya Belyaev <60135683+BelyaevIlyaUkr@users.noreply.github.com>
2020-04-12 22:10:11 +0300

    Third Assignment Final

--- Third Week And Third Assignment/React/confusion/src/components/MainComponent.js
+++ Third Week And Third Assignment/React/confusion/src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
-import DishDetail from './DishDetailedComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route , Redirect, withRouter } from 'react-router-dom';
